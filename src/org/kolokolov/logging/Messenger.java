e9fc27bc55096b974487f59a2e85bb2021321038
kolokolov <kolokolov_a@ukr.net>
2016-09-18 11:30:59 +0300

    Add testing for messenger

--- src/org/kolokolov/logging/Messenger.java
+++ src/org/kolokolov/logging/Messenger.java
@@ -11,24 +11,39 @@ import java.util.logging.Logger;
 
 public class Messenger {
     
-    private int currentHour = new GregorianCalendar().get(Calendar.HOUR_OF_DAY); 
-    private Locale locale = Locale.getDefault();
-    private String resourceFileName = "resources/Messages";
+    private String resourceFileName;
+    private int currentHour; 
+    private Locale locale;
+    
+    private String loggerConfigFileName;
     
     private static final Logger logger = Logger.getLogger("Messenger logger");
     
-    
-    
     public Messenger() {
+        this.resourceFileName = "resources/Messages";
+        this.currentHour = new GregorianCalendar().get(Calendar.HOUR_OF_DAY);
+        this.locale = Locale.getDefault();
+        this.loggerConfigFileName = "/resources/logging.properties";
+        configureLogger(this.loggerConfigFileName);
+    }
+    
+    public Messenger(String resourceFileName, String loggerConfigFileName, Locale locale, int currentHour) {
+        this.resourceFileName = resourceFileName;
+        this.loggerConfigFileName = loggerConfigFileName;
+        this.locale = locale;
+        this.currentHour = currentHour;
+        configureLogger(this.loggerConfigFileName);
+    }
+
+    private void configureLogger(String loggerConfigFileName) {
         try {
-            LogManager.getLogManager().readConfiguration(Messenger.class.getResourceAsStream("/resources/logging.properties"));
+            LogManager.getLogManager().readConfiguration(Messenger.class.getResourceAsStream(loggerConfigFileName));
         } catch (Exception ex) {
             System.err.println("Unable to setup logging configuration");
             ex.printStackTrace();
         }
-       
     }
-
+    
     private ResourceBundle bundleToResource(String resourceFileName, Locale locale) {
         ResourceBundle resource = null;
         try {
@@ -51,17 +66,17 @@ public class Messenger {
         return TimeOfDay.EVENING;
     }
 
-    public String getMessage(String resourceFileName, Locale locale, int hour) {
-        logger.log(Level.INFO, "Locale: {0}", locale);
-        ResourceBundle resource = bundleToResource(resourceFileName, locale);
+    public String getMessage() {
+        logger.log(Level.INFO, "Locale: {0}", this.locale);
+        ResourceBundle resource = bundleToResource(this.resourceFileName, this.locale);
         if (resource == null) return "Unable to get message!";
-        TimeOfDay timeOfDay = defineTimeOfDay(hour);
-        logger.log(Level.INFO, "Current hour: {0}, time of day: {1}", new Object[] {hour, timeOfDay});
+        TimeOfDay timeOfDay = defineTimeOfDay(this.currentHour);
+        logger.log(Level.INFO, "Current hour: {0}, time of day: {1}", new Object[] {currentHour, timeOfDay});
         return resource.getString(timeOfDay.toString());
     }
     
     public void printCurrentTimeMessage() {
-        System.out.println(getMessage(this.resourceFileName, this.locale, this.currentHour));
+        System.out.println(getMessage());
     }
     
     public static void main(String[] args) {
--- src/org/kolokolov/logging/tests/MessengerTest.java
+++ src/org/kolokolov/logging/tests/MessengerTest.java
@@ -14,39 +14,38 @@ import org.kolokolov.logging.Messenger;
 @RunWith(Parameterized.class)
 public class MessengerTest {
     
-    private String resourceFileName;
-    private Locale locale;
-    private int hour;
+    private static String resourceFileName = "resources/Messages";
+    private static String loggerFileName = "/resources/test_logging.properties";
+   
+    private Messenger messenger;
     private String expectegMessage;
     
-    public MessengerTest(String resourceFileName, Locale locale, int hour, String expectegMessage) {
-        this.resourceFileName = resourceFileName;
-        this.locale = locale;
-        this.hour = hour;
+    public MessengerTest(Messenger messenger, String expectegMessage) {
+        this.messenger = messenger;
         this.expectegMessage = expectegMessage;
     }
 
     @Parameters
     public static Collection<Object[]> parameters() {
         return Arrays.asList(new Object[][] {
-            {"resources/Messages", Locale.US, 0, "Good night!"},
-            {"resources/Messages", Locale.US, 8, "Good morning!"},
-            {"resources/Messages", Locale.US, 12, "Good afternoon!"},
-            {"resources/Messages", Locale.US, 19, "Good evening!"},
-            {"resources/Messages", Locale.forLanguageTag("ru"), 0, "Спокойной ночи!"},
-            {"resources/Messages", Locale.forLanguageTag("ru"), 7, "Доброе утро!"},
-            {"resources/Messages", Locale.forLanguageTag("ru"), 11, "Добрый день!"},
-            {"resources/Messages", Locale.forLanguageTag("ru"), 20, "Добрый вечер!"},
-            {"resources/Messages", Locale.GERMAN, 0, "Good night!"},
-            {"resources/Messages", Locale.ITALY, 8, "Good morning!"},
-            {"resources/Messages", Locale.FRANCE, 12, "Good afternoon!"},
-            {"resources/Messages", Locale.CHINA, 19, "Good evening!"},
-            {"resources/WrongFile", Locale.US, 0, "Unable to get message!"}
+            {new Messenger(resourceFileName, loggerFileName, Locale.US, 0), "Good night!"},
+            {new Messenger(resourceFileName, loggerFileName, Locale.US, 8), "Good morning!"},
+            {new Messenger(resourceFileName, loggerFileName, Locale.US, 12), "Good afternoon!"},
+            {new Messenger(resourceFileName, loggerFileName, Locale.US, 19), "Good evening!"},
+            {new Messenger(resourceFileName, loggerFileName, Locale.forLanguageTag("ru"), 0), "Спокойной ночи!"},
+            {new Messenger(resourceFileName, loggerFileName, Locale.forLanguageTag("ru"), 7), "Доброе утро!"},
+            {new Messenger(resourceFileName, loggerFileName, Locale.forLanguageTag("ru"), 11), "Добрый день!"},
+            {new Messenger(resourceFileName, loggerFileName, Locale.forLanguageTag("ru"), 20), "Добрый вечер!"},
+            {new Messenger(resourceFileName, loggerFileName, Locale.GERMAN, 0), "Good night!"},
+            {new Messenger(resourceFileName, loggerFileName, Locale.ITALY, 8), "Good morning!"},
+            {new Messenger(resourceFileName, loggerFileName, Locale.FRANCE, 12), "Good afternoon!"},
+            {new Messenger(resourceFileName, loggerFileName, Locale.CHINA, 19), "Good evening!"},
+            {new Messenger("resources/WrongFile", loggerFileName, Locale.US, 0), "Unable to get message!"}
         });
     }
     
     @Test
     public void testGetMessage() {
-        Assert.assertEquals(expectegMessage, new Messenger().getMessage(resourceFileName, locale, hour));
+        Assert.assertEquals(expectegMessage, messenger.getMessage());
     }
 }
