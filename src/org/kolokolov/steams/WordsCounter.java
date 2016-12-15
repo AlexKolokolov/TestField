4ca5c8e2ac559e64dc2e98bfcbb92b8188c6ae82
kolokolov <kolokolov_a@ukr.net>
2016-12-15 12:57:07 +0200

    Add WordCounter class to streams package
    The WordCounter class provides metheods for the most friquent
    words in a file counting.

---
+++ src/org/kolokolov/steams/WordsCounter.java
@@ -0,0 +1,40 @@
+package org.kolokolov.steams;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class WordsCounter {
+
+    public static void main(String[] args) {
+        WordsCounter wordsCounter = new WordsCounter();
+        String fileContent = null;
+        try {
+            fileContent = wordsCounter.readFile("./test/Shakespeare/THE TRAGEDY OF HAMLET, PRINCE OF DENMARK.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Map<String, Integer> words = wordsCounter.countWorgs(fileContent);
+        wordsCounter.printMostFriquentWords(words, 100);
+    }
+
+    public String readFile(String fileName) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(fileName)));
+    }
+
+    public Map<String, Integer> countWorgs(String str) {
+        Map<String, Integer> words = new HashMap<>();
+        Stream.of(str.split("\\PL+")).filter(word -> word.length() > 3).map(String::toLowerCase)
+                .forEach(word -> words.merge(word, 1, Integer::sum));
+        return words;
+    }
+
+    public void printMostFriquentWords(Map<String, Integer> words, int number) {
+        words.entrySet().stream().sorted((entry1, entry2) -> (entry2.getValue().compareTo(entry1.getValue())))
+                .limit(number).forEach(System.out::println);
+    }
+}
