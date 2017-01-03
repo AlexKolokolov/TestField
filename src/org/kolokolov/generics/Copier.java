918896e48fa945170538c25e2d205d43fea884a8
kolokolov <kolokolov_a@ukr.net>
2017-01-03 17:16:01 +0200

    Change generics demo class
    Generics demo class has been changed for
    better explanation of wild cards usage principals

--- src/org/kolokolov/generics/Copier.java
+++ src/org/kolokolov/generics/Copier.java
@@ -5,25 +5,22 @@ import java.util.List;
 
 import org.kolokolov.oop.Car;
 import org.kolokolov.oop.Transport;
-import org.kolokolov.oop.Truck;
-import org.kolokolov.oop.Vehicle;
 import org.kolokolov.oop.enums.Carcase;
-import org.kolokolov.oop.enums.TruckType;
 import org.kolokolov.oop.interfaces.Drivable;
 
 public class Copier {
-    public static <T extends Transport & Drivable> void copyList(List<T> src, List<? super Transport> out) {
-        src.forEach(out::add);
+    public static <T extends Transport & Drivable> void copyList(List<T> src, List<? super T> out) {
+        out.addAll(src);
     }
     
     public static void main(String[] args) {
-        List<Vehicle> cars = new ArrayList<>();
+        List<Car> cars = new ArrayList<>();
         cars.add(new Car("Toyota", "Camry", Carcase.SEDAN));
         cars.add(new Car("Skoda", "Octavia", Carcase.HATCHBACK));
         cars.add(new Car("BMW", "X5", Carcase.SUV));
-        cars.add(new Truck("MAN", "TGA", 18, TruckType.TRAKTOR));
+//        cars.add(new Truck("MAN", "TGA", 18, TruckType.TRAKTOR));
         
-        List<Transport> transports = new ArrayList<>();
+        List<Car> transports = new ArrayList<>();
         
         copyList(cars, transports);
         
